f15ef1367402df8831b73d4507444529310e5067
Эрик Вильданов <erik_bot@icloud.com>
2017-03-06 21:08:08 +0700

    Initial Commit

---
+++ TestFocusStart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestFocusStart
+//
+//  Created by Эрик Вильданов on 06.03.17.
+//  Copyright © 2017 Эрик Вильданов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TestFocusStartTests/TestFocusStartTests.swift
@@ -0,0 +1,36 @@
+//
+//  TestFocusStartTests.swift
+//  TestFocusStartTests
+//
+//  Created by Эрик Вильданов on 06.03.17.
+//  Copyright © 2017 Эрик Вильданов. All rights reserved.
+//
+
+import XCTest
+@testable import TestFocusStart
+
+class TestFocusStartTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
